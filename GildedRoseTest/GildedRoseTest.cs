53e80c68dbbb5b56a5053bc199f7e63e0fe38a74
linwenjun <linwenjun@163.com>
2020-11-07 10:29:23 +0000

    first commit

---
+++ GildedRoseTest/GildedRoseTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRose;
+
+namespace GildedRoseTest
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void Foo()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose.GildedRose app = new GildedRose.GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal("fixme", items[0].Name);
+        }
+    }
+}
